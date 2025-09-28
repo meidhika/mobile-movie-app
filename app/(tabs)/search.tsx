321dec764ffa387f7a95c613efc7c80df557d932
meidhika <meidhikanawa@gmail.com>
2025-09-28 21:03:21 +0700

    Trending Movies

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,6 +1,7 @@
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { fetchMovies } from "@/services/api";
+import { updateSearchCount } from "@/services/appwrite";
 import useFetch from "@/services/useFetch";
 import React, { useEffect, useState } from "react";
 import { ActivityIndicator, FlatList, Image, Text, View } from "react-native";
@@ -27,6 +28,13 @@ const Search = () => {
     }, 500);
     return () => clearTimeout(timeoutId);
   }, [searchQuery]);
+
+  useEffect(() => {
+    if (movies?.length > 0 && movies?.[0]) {
+      updateSearchCount(searchQuery, movies[0]);
+    }
+  }, [movies]);
+
   return (
     <View className="flex-1 bg-primary">
       <Image
